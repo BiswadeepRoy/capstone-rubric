98423e902b75af5ba523fcac02838d6a4cde934d
Biswadeep Roy <biswadeep.bibguru96@gmail.com>
2020-09-02 23:08:04 +0530

    Added full functionality to the server js file

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,131 @@
 //Importing different libraries
 const express = require('express');
+const dotenv = require('dotenv');
+const fetch = require('node-fetch');
+dotenv.config();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
+//Empty JS object to contain the data
+const projectData = {
+    days: 5,
+    city: 'pune'
+};
 
 //Setting up express server
 const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('dist'));
+
+
+//Rendering webpage on '/'
+app.get('/', function(req, res) {
+    res.sendFile(path.resolve('src/client/views/index.html'))
+})
+
+//Listening to port number
+const port = 8080;
+app.listen(port, function() {
+    console.log('App is listening on localhost:' + port);
+})
+
+//Post call
+app.post('/postdetails', handlePostDetails);
+
+// Callback function to complete POST '/postdata'
+function handlePostDetails(req, res) {
+
+    projectData = req.body;
+    console.log(projectData);
+}
+
+//Get call
+app.get('/getdetails', generateAPICallData);
+
+// Callback function to complete GET '/getdata'
+function generateAPICallData(req, res) {
+    console.log(projectData.city + " " + projectData.days);
+    getCountry(projectData.city).then(function(country) {
+        let days = projectData.days;
+        return getWeather('pune', country, days);
+    }).then(function(returnvalue) {
+        return getImage(returnvalue);
+    }).then(function(finalvalue) {
+        console.log(finalvalue);
+        return {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: finalvalue
+        };
+    }).then(function(response) {
+        console.log("The return value is " + JSON.stringify(response));
+        res.send(JSON.stringify(response));
+    });
+
+}
+
+//Async function to get country
+const getCountry = async(city) => {
+    const response = await fetch(`http://api.geonames.org/searchJSON?q=${city}&maxRows=1&username=biswa`);
+    try {
+        const responseData = await response.json();
+        const countryID = responseData.geonames[0].countryCode;
+        return countryID;
+
+    } catch (error) {
+        console.log(error);
+        return 'error';
+    }
+}
+
+//Async function to get weather
+const getWeather = async(city = '', country = '', days) => {
+    const response = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city},${country}&key=6bac312881784f5695dc61d05add4fa9&units=M&days=${days}`);
+    try {
+        const responseData = await response.json();
+        const requestedData = responseData.data[responseData.data.length - 1];
+        let date = new Date();
+        date.setDate(date.getDate() + days);
+        console.log(requestedData.max_temp);
+        return {
+            max_temp: requestedData.max_temp,
+            min_temp: requestedData.min_temp,
+            description: requestedData.weather.description,
+            city: city,
+            country: country,
+            date: date
+        };
+
+    } catch (error) {
+        console.log(error);
+        return 'error';
+    }
+}
+
+//Async function to get image
+const getImage = async(requestedData = {}) => {
+    let city = requestedData.city;
+    const response = await fetch(`https://pixabay.com/api/?key=18141304-163c887bda9b67802d25bb177&q=${city}&image_type=photo`);
+    try {
+        const responseData = await response.json();
+        const imageURL = responseData.hits[0].webformatURL;
+        return {
+            max_temp: requestedData.max_temp,
+            min_temp: requestedData.min_temp,
+            description: requestedData.description,
+            image_url: imageURL,
+            city: requestedData.city,
+            country: requestedData.country,
+            date: requestedData.date,
+        };
+
+    } catch (error) {
+        console.log(error);
+        return 'error';
+    }
+}
